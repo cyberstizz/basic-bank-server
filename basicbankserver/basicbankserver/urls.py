e2a7423c2ae436690bad939894cfda1249999a3a
Cyberstizz <charles.lamb.dev@gmail.com>
2023-01-30 12:41:54 -0500

    changed every path in urls.py by removing the /

--- basicbankserver/basicbankserver/urls.py
+++ basicbankserver/basicbankserver/urls.py
@@ -19,10 +19,10 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('/one', views.one, name='home'),
-    path('/add', views.add, name='home')
-    path('/delete', views.delete, name='delete')
-    path('/subtract', views.subtract, name='home')
+    path('one', views.one, name='home'),
+    path('add', views.add, name='home')
+    path('delete', views.delete, name='delete')
+    path('subtract', views.subtract, name='home')
 
 
 ]
