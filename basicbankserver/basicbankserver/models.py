38fca73efc59ad14c764ba2915219f43594ba827
Cyberstizz <charles.lamb.dev@gmail.com>
2023-01-30 16:20:19 -0500

    made migrations of the latest changes

--- basicbankserver/basicbankserver/models.py
+++ basicbankserver/basicbankserver/models.py
@@ -3,8 +3,8 @@ from django.core.validators import MinLengthValidator, MaxLengthValidator
 
 
 class accounts(models.Model):
-    account_number = models.IntegerField(validators=[MaxLengthValidator(6),MinLengthValidator(6)], unique=True)
-    account_balance = models.IntegerField(max_length=10)
+    account_number = models.IntegerField(unique=True)
+    account_balance = models.IntegerField(validators=[MaxLengthValidator(10)])
     account_name = models.CharField(max_length=30)
 
     class Meta:
