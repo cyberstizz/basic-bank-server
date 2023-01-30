9481125928f45d0152a127f913dc24c5d73bd05b
Cyberstizz <charles.lamb.dev@gmail.com>
2023-01-30 16:21:11 -0500

    edited import statement

--- basicbankserver/basicbankserver/models.py
+++ basicbankserver/basicbankserver/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.core.validators import MinLengthValidator, MaxLengthValidator
+from django.core.validators import MaxLengthValidator
 
 
 class accounts(models.Model):
