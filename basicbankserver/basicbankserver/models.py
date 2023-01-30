342426945ae698bfa50a949b9a0adb6a84fd890b
Cyberstizz <charles.lamb.dev@gmail.com>
2023-01-30 10:08:56 -0500

    tried adding instances but for some reason django is now working how I expected. will come back later

--- basicbankserver/basicbankserver/models.py
+++ basicbankserver/basicbankserver/models.py
@@ -4,3 +4,6 @@ class accounts(models.Model):
     account_number = models.CharField(max_length=30)
     account_balance = models.CharField(max_length=30)
     account_name = models.CharField(max_length=30)
+
+    class Meta:
+        app_label = 'basicbankserver'
