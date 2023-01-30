971ef3dd4a16e82b9af9e1addd1a07861759fdb6
Cyberstizz <charles.lamb.dev@gmail.com>
2023-01-30 09:00:27 -0500

    added the required views in views.pyy

--- basicbankserver/basicbankserver/views.py
+++ basicbankserver/basicbankserver/views.py
@@ -2,4 +2,21 @@ from django.shortcuts import render
 
 
 def home(request):
+    #this view will return every account
     return render('hello stizz')
+
+
+def one(request):
+    pass
+
+
+def add(request):
+    pass
+
+
+def delete_one(request):
+    pass
+
+#this view will subtract from a balance
+def subtract(request):
+    pass
