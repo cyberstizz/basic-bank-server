03bfb2ba7f716036e87793c732b515887662ba78
Cyberstizz <charles.lamb.dev@gmail.com>
2023-01-30 16:15:24 -0500

    made changes to the accounts model by adding integer fields instead of the text fields that I had

--- basicbankserver/basicbankserver/models.py
+++ basicbankserver/basicbankserver/models.py
@@ -1,8 +1,10 @@
 from django.db import models
+from django.core.validators import MinLengthValidator, MaxLengthValidator
+
 
 class accounts(models.Model):
-    account_number = models.CharField(max_length=30)
-    account_balance = models.CharField(max_length=30)
+    account_number = models.IntegerField(validators=[MaxLengthValidator(6),MinLengthValidator(6)], unique=True)
+    account_balance = models.IntegerField(max_length=10)
     account_name = models.CharField(max_length=30)
 
     class Meta:
