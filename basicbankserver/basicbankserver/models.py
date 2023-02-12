9cdaafdc750924b665e65cb99f0569b4c6eacaf5
Cyberstizz <charles.lamb.dev@gmail.com>
2023-02-12 02:45:36 -0500

    added abstract user

--- basicbankserver/basicbankserver/models.py
+++ basicbankserver/basicbankserver/models.py
@@ -1,5 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
+
+
+class User(AbstractUser):
+    pass
 
 class customers(models.Model):
     name = models.CharField(max_length=30)
