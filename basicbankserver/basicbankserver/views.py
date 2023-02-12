0827f5e46628dedc87c8364b8a8a2f3b3db0dc18
Cyberstizz <charles.lamb.dev@gmail.com>
2023-02-11 21:27:41 -0500

    added some new accounts to sqlite file and changed one view name to getOne just to be more descriptive

--- basicbankserver/basicbankserver/views.py
+++ basicbankserver/basicbankserver/views.py
@@ -24,7 +24,7 @@ def home(request):
 
 
 
-def one(request, name):
+def getOne(request, name):
     #this view will return one account
     the_account = customers.objects.first()
     #this creates a list to store everything into
@@ -104,7 +104,7 @@ def create(request, name, email, customer_ID, account_number, account_balance, a
         new_customer.save()
         
         
-        new_account = accounts.objects.create(account_number=account_number, account_balance=account_balance, account_type=account_type, customer_ID=customer_ID)
+        new_account = accounts.objects.create(account_number=account_number, account_balance=account_balance, account_type=account_type, customer_ID=new_customer)
         new_account.save()
         return HttpResponse(f'this is the newly created account number{customer_ID}, and this is the new account{new_account}')
 
