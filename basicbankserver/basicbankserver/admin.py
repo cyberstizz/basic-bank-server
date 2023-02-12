fed2c65efefa71a285ec896db63adef43f0dc352
Cyberstizz <charles.lamb.dev@gmail.com>
2023-02-12 02:44:55 -0500

    changed admin.py by adding user which I haven't creaed yet

--- basicbankserver/basicbankserver/admin.py
+++ basicbankserver/basicbankserver/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
-from basicbankserver.models import customers, accounts
+from basicbankserver.models import customers, accounts, User
+from django.contrib.auth.admin import UserAdmin
 
 
 admin.site.register(customers)
 
 admin.site.register(accounts)
+
+admin.site.register(User, UserAdmin)
