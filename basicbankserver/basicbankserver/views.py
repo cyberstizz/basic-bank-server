5ff3fcba35bba8b040edc204990a13f5c0ffc191
Cyberstizz <charles.lamb.dev@gmail.com>
2023-02-28 05:24:51 -0500

    lots of changes to the create view

--- basicbankserver/basicbankserver/views.py
+++ basicbankserver/basicbankserver/views.py
@@ -166,7 +166,7 @@ def create(request):
 
         print('these are the items just sent')
         print(data['name'])
-        name = data['name']
+        username = data['username']
         print(data['password'])
         password = data['password']
         print(data['email'])
@@ -180,13 +180,13 @@ def create(request):
         account_number = random.randint(100, 999)
 
 
-        new_user = User.objects.create(username=name, password=password)
+        new_user = User.objects.create(username=username, password=password)
         new_user.save()
         
         
         new_account = accounts.objects.create(account_number=account_number, account_balance=deposit, account_type=account, user=new_user)
         new_account.save()
-        return HttpResponse(f'this is the newly created account number{username}, and this is the new account{new_account}')
+        return HttpResponse(f'this is the newly created account number{username}, and this is the new account{account_number}')
 
 
 
